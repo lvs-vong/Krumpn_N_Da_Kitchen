3155ef729b74c86223d5ccb4975e7c96928f9758
lvs-vong <evong@nevada.unr.edu>
2022-07-11 19:37:31 -0700

    changed to component to be dynamic

--- assets/js/components/RecipeCard.jsx
+++ assets/js/components/RecipeCard.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 
-const RecipeCard = () => {   
+const RecipeCard = ({recipe}) => {   
     return (
-        <div className="relative bg-main h-60 w-96 border-8 border-zinc-50 rounded-md">
+        <div className="relative bg-main h-60 w-96 border-double border-8 border-zinc-50 rounded-md">
             <button className="absolute h-full w-full bg-slate-900 opacity-0 hover:opacity-80"
                     style={{zIndex: '10000'}}>
                 <div className="absolute w-full h-full text-white ml-0 top-0 p-3">
-                    <h1 className="text-2xl underline">Chicken Nuggets</h1>
                     <div className="relative h-32 overflow-y-auto pt-2">
-                        <p className="p-2 border-2 border-zinc-50">A super delicious description of super delicous chicken nuggets made by me!</p>
+                        <p className="p-2 border-2 border-zinc-50 font-eightbit">{recipe.description}</p>
                     </div>
-                    <p className="absolute bottom-2 right-3 font-semibold">Prep Time: 1 hour</p>
+                    <p className="absolute bottom-2 right-3 font-semibold">{recipe.prepTime}</p>
                 </div>
 
             </button>
             <div className="flex justify-center p-2">
-               <img src = "https://via.placeholder.com/200"/> 
+               <img className="max-h-52" src ={recipe.picture}/> 
             </div>
             <div className="absolute w-full h-16 bottom-0">
                 <div className="absolute h-full w-full bg-slate-900 opacity-75" style={{zindex: '-1'}}></div>
-                <div style={{zindex: '1'}}><h1 className="absolute p-2 mt-2 text-tertiary text-2xl font-bold">Chicken Nuggets</h1></div>
+                <div style={{zindex: '1'}}><h1 className="absolute p-2 mt-2 text-tertiary text-1xl font-bold font-osa">{recipe.name}</h1></div>
             </div>
         </div>
     );
