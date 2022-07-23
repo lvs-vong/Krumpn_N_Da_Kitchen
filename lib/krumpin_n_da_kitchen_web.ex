c6b8d5f992daffa16edda0c6aacfe7fd0de98d13
lvs-vong <evong@nevada.unr.edu>
2022-07-23 03:45:05 -0700

    adding the checkbox helper

--- lib/krumpin_n_da_kitchen_web.ex
+++ lib/krumpin_n_da_kitchen_web.ex
@@ -88,7 +88,7 @@ defmodule KrumpinNDaKitchenWeb do
     quote do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
-
+      import KrumpinNDaKitchenWeb.CheckboxHelper
       import KrumpinNDaKitchenWeb.InputHelpers
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
